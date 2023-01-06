978e38a67be67d6015e3bcf1793647db5ec1226e
Oleksandr Husliakov <110162593+AlexGusl97@users.noreply.github.com>
2023-01-06 23:40:31 +0200

    Initial commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+  )
+  .join("");
+
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener("click", onPreviewClick);
+
+function onPreviewClick(event) {
+  event.preventDefault();
+}
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
